424f93e9d9d02c05569abab0f08dc0afe3d04283
Debora Oliveira <debora.ramos.2@outlook.com>
2022-09-10 18:05:40 -0300

    Adicionar arquivos de projeto.

---
+++ POO-1/Filme.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_1
+{
+    internal class Filme
+    {
+        //atributos
+        public string nome;
+        public string descricao;
+        public int ano;
+        public string studio;
+        public List<string> atores = new List<string>();
+
+        static public string plataforma = "NETFLIX";
+
+        //metodos
+        public void Play()
+        {
+            Console.WriteLine($"Rodando o filme: {nome}");
+        }
+
+        //utilizando static
+        static public void Pause()
+        {
+            Console.WriteLine(plataforma);
+            Console.WriteLine("||");
+        }
+
+        //construtor
+        public Filme(string nome, string descricao, int ano, string studio)
+        {
+            this.nome = nome;
+            this.descricao = descricao;
+            this.ano = ano;
+            this.studio = studio;
+
+            Console.WriteLine("Deu certo\n");
+        }
+    }
+}
---
+++ POO-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //criação de var
+            //Filme filme = new Filme();
+            //acessar os campos do filme
+            //filme.nome = "Procurando Nemo";
+            //filme.Play();
+
+
+            //usando static
+            Filme.Pause();
+            //Filme.plataforma = "Amazon Prime";
+
+
+            //usando construtor
+            Filme filme = new Filme("Madagascar", "Filme - infantil muito show", 2006, "DreamWorks");
+            Filme CodigoMorse = new Filme("CodigoMorse", "Filme drama muito legal", 2, "ABK");
+
+            /*
+            Console.WriteLine(filme.nome);
+            Console.WriteLine(filme.descricao);
+            */
+
+
+            Console.ReadLine();
+        }
+    }
+}
